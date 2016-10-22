c62049f1b9a54d8f73ca784a101b3fc835b076bb
Iggi <iggirex@gmail.com>
2016-10-22 11:10:43 -0600

    init commit

--- Hella Wurl/ViewController.swift
+++ Hella Wurl/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+
+    @IBOutlet weak var titleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomePressed(_ sender: AnyObject) {
+        background.isHidden = false
+        titleButton.isHidden = false
+    }
 
 }
 
